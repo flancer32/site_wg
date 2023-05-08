82f9ef30e9ff46b263dbcc4c36ea88af29a7a3f4
Alex Gusev <alex@flancer64.com>
2023-05-08 09:43:21 +0300

    OWN-295 Add 404 handler for the site

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -1,9 +1,15 @@
 import {defineConfig} from 'astro/config';
-import vue from "@astrojs/vue";
+import vue from '@astrojs/vue';
+
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
     outDir: '../web',
     // Vue 3 App customization
-    integrations: [vue({appEntrypoint: '/src/_app'})]
+    integrations: [
+        vue({appEntrypoint: '/src/_app'}),
+        sitemap(),
+    ],
+    site: 'https://wiredgeese.com',
 });
